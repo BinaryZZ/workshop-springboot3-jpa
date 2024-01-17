c5bf7917b474cf0b9946bae66fbd59e0d21ecd3d
Jonathan Ricardo <jonathan_levelup@outlook.com>
2024-01-17 01:44:28 -0300

    JPA repository, dependency injection, database seending

---
+++ src/main/java/com/binaryz/course/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.binaryz.course.repositories;
+
+import com.binaryz.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
