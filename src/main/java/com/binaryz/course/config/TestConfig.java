9cd13c9d39d241d181bfe427f6ea86f6c5bed0ae
Jonathan Ricardo <jonathan_levelup@outlook.com>
2024-01-18 01:19:32 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/binaryz/course/config/TestConfig.java
+++ src/main/java/com/binaryz/course/config/TestConfig.java
@@ -1,15 +1,9 @@
 package com.binaryz.course.config;
 
 
-import com.binaryz.course.entities.Category;
-import com.binaryz.course.entities.Order;
-import com.binaryz.course.entities.Product;
-import com.binaryz.course.entities.User;
+import com.binaryz.course.entities.*;
 import com.binaryz.course.entities.enums.OrderStatus;
-import com.binaryz.course.repositories.CategoryRepository;
-import com.binaryz.course.repositories.OrderRepository;
-import com.binaryz.course.repositories.ProductRepository;
-import com.binaryz.course.repositories.UserRepository;
+import com.binaryz.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +27,9 @@ public class TestConfig implements CommandLineRunner { //implementation to use C
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -68,6 +65,13 @@ public class TestConfig implements CommandLineRunner { //implementation to use C
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
     }
 }
---
+++ src/main/java/com/binaryz/course/repositories/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.binaryz.course.repositories;
+
+import com.binaryz.course.entities.Order;
+import com.binaryz.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> { // Interface for dependency injection
+}
